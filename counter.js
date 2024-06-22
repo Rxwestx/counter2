e740eb583d8628be6c9350f680b01938f263c8f7
Rxwestx <wexriexst13@gmail.com>
2024-06-22 20:51:54 +0900

    +10カウント-10カウント実装

--- counter.js
+++ counter.js
@@ -6,11 +6,17 @@
         let currentCount = parseInt($counter.textContent);
         if($targetButton.textContent === "+"){
             $counter.textContent = currentCount + 1;
-        } else {
+        }
+        else if($targetButton.textContent === "+10") {
+            $counter.textContent = currentCount + 10;
+        }
+        else if($targetButton.textContent === "-10") {
+            $counter.textContent = currentCount - 10;
+        }
+        else{
             $counter.textContent = currentCount - 1;
         }
-    }
-
+    };
     for(let index = 0; index < document.getElementsByClassName("js-button").length; index++) {
         document.getElementsByClassName("js-button")[index].addEventListener("click", (e) => clickHandler(e))
     }
