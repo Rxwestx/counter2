473d31b99e0883b3e9ca2932ba670ac4ea179d5f
Rxwestx <wexriexst13@gmail.com>
2024-06-23 13:09:29 +0900

    ０より下にカウントしないように実装

--- counter.js
+++ counter.js
@@ -9,20 +9,30 @@
     } else if ($targetButton.textContent === "+10") {
       $counter.textContent = currentCount + 10;
     } else if ($targetButton.textContent === "-10") {
-      $counter.textContent = currentCount - 10;
-    } else {
-      $counter.textContent = currentCount - 1;
-    }
-  };
+  //     $counter.textContent = currentCount - 10;
+  //   } else {
+  //     $counter.textContent = currentCount - 1;
+  //   }
+  // };
+  // 0より下に行かないように設定
+  $counter.textContent = Math.max(0, currentCount - 10);
+  if (currentCount - 10 < 0) {
+    loadVideo();
+  }
+} else {
+  // 0より下に行かないように設定
+  $counter.textContent = Math.max(0, currentCount - 1);
+  if (currentCount - 1 < 0) {
+    loadVideo();
+  }
+}
+};
   for (
     let index = 0;
     index < document.getElementsByClassName("js-button").length;
     index++
   ) {
-    document
-      .getElementsByClassName("js-button")
+    document.getElementsByClassName("js-button")
       [index].addEventListener("click", (e) => clickHandler(e));
   }
 })();
-
-// location.href = "./video/video.mp4";
